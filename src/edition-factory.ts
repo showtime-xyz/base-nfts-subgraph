823eccc14828ead0fdc8e7f004db06cd3eb798a8
Jorge Valdez <jorge@yung.dev>
2023-09-13 15:35:09 -0500

    Initialize base subgraph

--- src/edition-factory.ts
+++ src/edition-factory.ts
@@ -1,18 +1,22 @@
-import { CreatedEdition as CreatedEditionEvent } from "../generated/EditionFactory/EditionFactory"
-import { CreatedEdition } from "../generated/schema"
+import { Edition } from "../generated/EditionFactory/Edition";
+import { CreatedEdition as CreatedEditionEvent } from "../generated/EditionFactory/EditionFactory";
+import { FreeNFTDrop } from "../generated/schema";
+import { Edition as EditionTemplate } from "../generated/templates";
 
-export function handleCreatedEdition(event: CreatedEditionEvent): void {
-  let entity = new CreatedEdition(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.editionId = event.params.editionId
-  entity.creator = event.params.creator
-  entity.editionContractAddress = event.params.editionContractAddress
-  entity.tags = event.params.tags
+export default function handleCreatedEdition(event: CreatedEditionEvent): void {
+  let collectionAddress = event.params.editionContractAddress;
+  let edition = Edition.bind(collectionAddress);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let entity = new FreeNFTDrop(collectionAddress);
+  entity.createdAt = event.block.timestamp;
+  entity.creator = event.params.creator;
+  entity.editionSize = edition.editionSize();
+  entity.name = edition.name();
+  entity.description = edition.description();
+  entity.imageUrl = edition.imageUrl();
+  entity.animationUrl = edition.animationUrl();
+  entity.deadline = edition.endOfMintPeriod();
+  entity.save();
 
-  entity.save()
+  EditionTemplate.create(collectionAddress);
 }
